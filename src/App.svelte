72e14cd61b8198c18780908478193ca6c6599333
mindtriik <101913596+mindtriik@users.noreply.github.com>
2022-04-06 23:52:54 -0400

    Completed the Form

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,19 @@
 <script>
- 
+  import Table from "./lib/Table.svelte";
+  import Form from "./lib/Form.svelte";
 </script>
 
 <main class="section">
+  <div class="container">
+    <div class="columns">
+      <div class="column is-four-fifths">
+        <h1 class="title is-1">Project Estimator</h1>
+        
+        <Form />
 
-  <h1 class="title">Hello world!</h1>
-
-  <button class="button is-dark">button</button>
-
+        <Table />
+        
+      </div>
+    </div>
+  </div>
 </main>
-
---
+++ src/lib/Table.svelte
@@ -0,0 +1,1 @@
+<h2 class="title is-2">Table Component</h2>
